680d3008bd2a01093bc6a537ac078967de5ed866
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-02-16 18:13:06 +0100

    lekcja 5

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Portal otherPortal;
+    [SerializeField] Material material;
+
+    public Camera myCamera;
+
+    public Transform renderSurface;
+    public Transform portalCollider;
+
+
+    private GameObject player;
+    private PortalTeleport portalTeleport;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        portalTeleport = portalCollider.GetComponent<PortalTeleport>();
+        portalTeleport.player = player.transform;
+        portalTeleport.receiver = otherPortal.portalCollider;
+    }
+
+}
